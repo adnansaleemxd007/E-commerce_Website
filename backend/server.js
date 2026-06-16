e3ed9d6b6c6ab33aa6cdfdc0458fa931bea78798
adnansaleemxd007 <adnanjuttxd07@gmail.com>
2026-06-16 10:22:08 -0700

    feat: initialize Express server with MongoDB connection and static asset serving

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/api/health', (req, res) => res.send('API is running...'));
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
